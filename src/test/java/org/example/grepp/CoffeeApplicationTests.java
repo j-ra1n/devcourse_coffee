c64c886a2e3cae1ed87cc1b928b3c32adb5665eb
j-ra1n <j_ra1n@naver.com>
2024-09-08 21:15:28 +0900

    setting

---
+++ src/test/java/org/example/grepp/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grepp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
