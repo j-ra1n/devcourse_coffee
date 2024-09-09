f096a20b89e3b3f67020d6f0028eb62ece467c64
j-ra1n <j_ra1n@naver.com>
2024-09-09 14:38:52 +0900

    refactor

--- src/main/java/org/example/grepp/controller/order/request/OrderUpdateRequest.java
+++ src/main/java/org/example/grepp/controller/order/request/OrderUpdateRequest.java
@@ -1,12 +1,16 @@
 package org.example.grepp.controller.order.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.grepp.controller.orderitem.request.OrderItemRequest;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderUpdateRequest {
 
     @NotNull
